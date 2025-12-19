d4d6bf1fcfd62574ac67cf62cf4310acfce22412
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-12-19 07:08:33 +0700

    update information card

--- src/components/card/Information.jsx
+++ src/components/card/Information.jsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { FaCalendarAlt, FaClock, FaBell } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { formatWaktu } from "@/lib/time";
 import { Skeleton } from "@/components/ui/Skeleton";
+import { ModalInformation } from "@/components/ui/Modal";
 
 export default function Information({ type, data, loading = false }) {
+  const [selectedItem, setSelectedItem] = useState(null);
+
   const icon =
     type === "agenda" ? (
       <FaCalendarAlt className="text-lg" />
@@ -22,13 +25,8 @@ export default function Information({ type, data, loading = false }) {
     type === "agenda" ? "Agenda" : type === "berita" ? "Berita" : "Pengumuman";
 
   const getLink = (item) => {
-    if (type === "agenda") {
-      return `/agenda`;
-    }
-
-    if (type === "berita") {
-      return `/articles/${item.slug}`;
-    }
+    if (type === "agenda") return `/agenda`;
+    if (type === "berita") return `/articles/${item.slug}`;
     return "#";
   };
 
@@ -82,35 +80,11 @@ export default function Information({ type, data, loading = false }) {
                   : ""
               }`}
             >
-              <Link href={getLink(item)} className="block">
-                {idx === 0 && (type === "agenda" || type === "berita") ? (
-                  <div className="relative w-full h-60 rounded-lg overflow-hidden">
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_HOST}${item.image_path}`}
-                      alt={item.title}
-                      fill
-                      className="object-cover"
-                    />
-
-                    <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
-                    <div className="absolute bottom-2 p-2 ml-2 text-white border-l-2 border-l-primary">
-                      <h3 className="text-lg font-semibold line-clamp-2">
-                        {item.title}
-                      </h3>
-                      <div className="flex items-center gap-3 text-md mt-1">
-                        <span className="flex items-center gap-1">
-                          <FaCalendarAlt />{" "}
-                          <time dateTime={item.date}>
-                            {formatWaktu(item.date, "date")}
-                          </time>
-                        </span>
-                        <span className="flex items-center gap-1">
-                          <FaClock /> {formatWaktu(item.date, "time")}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                ) : type === "pengumuman" ? (
+              {type === "pengumuman" ? (
+                <button
+                  onClick={() => setSelectedItem(item)}
+                  className="w-full text-left"
+                >
                   <div className="flex flex-col h-full bg-neut-50 p-4 rounded-md">
                     <h3 className="text-md font-medium text-neut-900 line-clamp-2">
                       {item.title}
@@ -120,35 +94,73 @@ export default function Information({ type, data, loading = false }) {
                       <time dateTime={item.date}>{formatWaktu(item.date)}</time>
                     </div>
                   </div>
-                ) : (
-                  <div className="flex items-start gap-3">
-                    <div className="flex flex-col items-center border border-neut-300 rounded-md overflow-hidden min-w-14 min-h-14">
-                      <div className="bg-primary border border-primary text-white w-full text-center py-1 text-[10px] font-semibold uppercase">
-                        {new Date(item.date).toLocaleDateString("en-US", {
-                          month: "short",
-                        })}
-                      </div>
-                      <div className="bg-white text-neut-900 text-sm font-bold py-1 w-full text-center">
-                        {new Date(item.date).getDate()}
+                </button>
+              ) : (
+                <Link href={getLink(item)} className="block">
+                  {idx === 0 && (type === "agenda" || type === "berita") ? (
+                    <div className="relative w-full h-60 rounded-lg overflow-hidden">
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_HOST}${item.image_path}`}
+                        alt={item.title}
+                        fill
+                        className="object-cover"
+                      />
+                      <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+                      <div className="absolute bottom-2 p-2 ml-2 text-white border-l-2 border-l-primary">
+                        <h3 className="text-lg font-semibold line-clamp-2">
+                          {item.title}
+                        </h3>
+                        <div className="flex items-center gap-3 text-md mt-1">
+                          <span className="flex items-center gap-1">
+                            <FaCalendarAlt />{" "}
+                            <time dateTime={item.date}>
+                              {formatWaktu(item.date, "date")}
+                            </time>
+                          </span>
+                          <span className="flex items-center gap-1">
+                            <FaClock /> {formatWaktu(item.date, "time")}
+                          </span>
+                        </div>
                       </div>
                     </div>
+                  ) : (
+                    <div className="flex items-start gap-3">
+                      <div className="flex flex-col items-center border border-neut-300 rounded-md overflow-hidden min-w-14 min-h-14">
+                        <div className="bg-primary border border-primary text-white w-full text-center py-1 text-[10px] font-semibold uppercase">
+                          {new Date(item.date).toLocaleDateString("en-US", {
+                            month: "short",
+                          })}
+                        </div>
+                        <div className="bg-white text-neut-900 text-sm font-bold py-1 w-full text-center">
+                          {new Date(item.date).getDate()}
+                        </div>
+                      </div>
 
-                    <div className="flex flex-col">
-                      <h3 className="text-md font-medium text-neut-900 line-clamp-2 h-1/2">
-                        {item.title}
-                      </h3>
-                      <div className="flex items-center gap-2 text-xs font-semibold text-primary mt-2 h-1/2">
-                        <FaClock />{" "}
-                        <time dateTime={item.date}>
-                          {formatWaktu(item.date)}
-                        </time>
+                      <div className="flex flex-col">
+                        <h3 className="text-md font-medium text-neut-900 line-clamp-2 h-1/2">
+                          {item.title}
+                        </h3>
+                        <div className="flex items-center gap-2 text-xs font-semibold text-primary mt-2 h-1/2">
+                          <FaClock />{" "}
+                          <time dateTime={item.date}>
+                            {formatWaktu(item.date)}
+                          </time>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </Link>
+                  )}
+                </Link>
+              )}
             </article>
           ))}
+
+        {type === "pengumuman" && selectedItem && (
+          <ModalInformation
+            isOpen={!!selectedItem}
+            data={selectedItem}
+            onClose={() => setSelectedItem(null)}
+          />
+        )}
       </div>
     </section>
   );
